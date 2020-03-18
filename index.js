54c0023eabaae5a84083fd1191f6b93ca18e6d5d
Matthew Enbar <matthew.enbar@gmail.com>
2020-03-18 01:57:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-function add(){
-
-}
-
-function subtract(){
-
-}
-
-function multiple(){
-
-}
-
-function divide(){
-
-}
 
 function add(a,b){
   return a + b
+}
+
+function subtract(a,b){
+  return a - b
+}
+
+
+function multiple(a,b){
+return a*b
+}
+
+function divide(a,b){
+return a/b
 }
